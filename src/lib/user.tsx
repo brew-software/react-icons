e647272e438fa348ad3c60fea5f77623d9825b21
Natalie <natalie.coleman39@gmail.com>
2021-03-14 16:27:49 -0400

    Add a few more options for user icon

--- src/lib/user.tsx
+++ src/lib/user.tsx
@@ -1,22 +1,26 @@
-import { createFontawesomeIcon } from "./+fontawesomeIcon";
-import {
-  faUser as solid,
-  faUserCircle as solidCircle,
-  faUserCheck as solidCheck,
-  faUserTimes as solidTimes,
-} from "@fortawesome/free-solid-svg-icons";
 import {
   faUser as regular,
   faUserCircle as regularCircle,
 } from "@fortawesome/free-regular-svg-icons";
+import {
+  faUser as solid,
+  faUserCheck as solidCheck,
+  faUserCircle as solidCircle,
+  faUserLock as solidLock,
+  faUserPlus as solidPlus,
+  faUserTimes as solidTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import { createFontawesomeIcon } from "./+fontawesomeIcon";
 
 const UserIcon = createFontawesomeIcon({
   solid,
-  regular,
-  solidCircle,
-  regularCircle,
   solidCheck,
+  solidCircle,
+  solidLock,
+  solidPlus,
   solidTimes,
+  regular,
+  regularCircle,
 });
 
 export default UserIcon;
