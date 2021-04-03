859bc8ca9f985bd8f1e2afc75e7083f58b09d6df
Natalie <natalie.coleman39@gmail.com>
2021-04-03 15:06:56 -0400

    Add flip and rotate to font awewsome icons

--- src/lib/+fontawesomeIcon.tsx
+++ src/lib/+fontawesomeIcon.tsx
@@ -1,27 +1,34 @@
 import { CSSProperties, FunctionComponent } from "react";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  FontAwesomeIcon,
+  FontAwesomeIconProps,
+} from "@fortawesome/react-fontawesome";
 
 type IconProps<T> = {
-	type: keyof T;
-	color?: string;
-	className?: string;
-	style?: CSSProperties;
+  type: keyof T;
+  color?: string;
+  className?: string;
+  style?: CSSProperties;
+  flip?: FontAwesomeIconProps["flip"];
+  rotate?: FontAwesomeIconProps["rotate"];
 };
 
 export function createFontawesomeIcon<
-	T extends { [type: string]: IconDefinition }
+  T extends { [type: string]: IconDefinition }
 >(icons: T): FunctionComponent<IconProps<T>> {
-	return ({ type, color, className, style }: IconProps<T>) => {
-		const definition = icons[type];
+  return ({ type, color, className, style, rotate, flip }: IconProps<T>) => {
+    const definition = icons[type];
 
-		return (
-			<FontAwesomeIcon
-				icon={definition}
-				color={color}
-				className={className}
-				style={style}
-			/>
-		);
-	};
+    return (
+      <FontAwesomeIcon
+        icon={definition}
+        color={color}
+        className={className}
+        style={style}
+        rotate={rotate}
+        flip={flip}
+      />
+    );
+  };
 }
